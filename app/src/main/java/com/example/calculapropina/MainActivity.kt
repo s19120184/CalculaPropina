5aaeee90c29aa971a75bfb6d562854946e031ba2
luis alberto Alvarez <luisslaycore@gmail.com>
2023-09-12 01:01:35 -0600

    editNumber

--- app/src/main/java/com/example/calculapropina/MainActivity.kt
+++ app/src/main/java/com/example/calculapropina/MainActivity.kt
@@ -3,12 +3,16 @@ package com.example.calculapropina
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.textInputServiceFactory
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.calculapropina.ui.theme.CalculaPropinaTheme
 
@@ -30,6 +34,17 @@ class MainActivity : ComponentActivity() {
 }
 @Composable
 fun TipTimeLayout(){
+
+}
+
+@Composable
+fun EditNumber(
+    @StringRes label:Int,
+    value: String,
+    onValueChange: (String)->Unit,
+    modifier: Modifier = Modifier
+){
+  // EditNumber()
 
 }
 
